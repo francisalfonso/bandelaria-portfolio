7518004a7cbdc9eb2bb35e6780a6c51be32ebbd2
Francis Alfonso L. Bandelaria <fabandelaria.itso@tip.edu.ph>
2025-07-11 02:43:25 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/francis-portfolio",
+  base: "/bandelaria-portfolio",
   css: {
     modules: {
       localsCovention: "camelCase"
